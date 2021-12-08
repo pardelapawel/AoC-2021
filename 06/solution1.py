0c9551181abeed22a763b0cf19c6db30ddfc0928
파벨 <pardela.pawel@gmail.com>
2021-12-08 19:47:47 +0100

    solved 06 part 1

--- 06/solution1.py
+++ 06/solution1.py
@@ -1,8 +1,22 @@
 #!/bin/python
 import sys
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def calculate_children(fish_timer, time_left):
+    if time_left < 0:
+        return 0
+    if time_left == 0:
+        return 1
+    if fish_timer == 0:
+        return calculate_children(8, time_left - 1) + calculate_children(6, time_left - 1)
+    return calculate_children(fish_timer - 1, time_left - 1)
 
 def main():
     with open(sys.argv[1]) as input_file:
+        fishes = [int(x) for x in input_file.readline().strip().split(',')]
+        print(fishes)
+        print(sum([calculate_children(x, 80) for x in fishes]))
 
 
 
