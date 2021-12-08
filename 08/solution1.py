31a86def6a04cb02bccc487cdd2cbe28c046fa97
파벨 <pardela.pawel@gmail.com>
2021-12-08 20:59:20 +0100

    solved 08

--- 08/solution1.py
+++ 08/solution1.py
@@ -1,10 +1,22 @@
 #!/bin/python
 import sys
 
+len2digit = {
+        2: 1,
+        4: 4,
+        3: 7,
+        7: 8
+        }
 
 def main():
     with open(sys.argv[1]) as input_file:
-        pass
+        res = 0
+        for line in input_file.readlines():
+            in_digits, out_digits = line.strip().split('|')
+            out_digits = out_digits.split()
+            known = [len(d) for d in out_digits if len(d) in len2digit]
+            res += len(known)
+        print(res)
 
 
 
