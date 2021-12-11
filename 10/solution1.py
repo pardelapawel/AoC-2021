1f0031edd68cff16ff7ad14ca1ff87a1cec8b3e5
파벨 <pardela.pawel@gmail.com>
2021-12-12 00:54:17 +0100

    solved 10 part 1

--- 10/solution1.py
+++ 10/solution1.py
@@ -1,9 +1,61 @@
 #!/bin/python
 import sys
 
+def spaced(arr):
+    print(' '.join([str(x).rjust(2) for x in arr]))
+
 def main():
     with open(sys.argv[1]) as input_file:
-        pass
+        bracket_lookup = {
+                '(': ')',
+                '[': ']',
+                '{': '}',
+                '<': '>',
+                ')': '(',
+                ']': '[',
+                '}': '{',
+                '>': '<'
+                }
+        point_lookup = {
+                ')': 3,
+                ']': 57,
+                '}': 1197,
+                '>': 25137
+                }
+        o_brackets = set('( [ { <'.split())
+        c_brackets = set(') ] } >'.split())
+        points = 0
+        for line in input_file.readlines():
+            #print(len(line))
+            #print()
+            line = line.strip()
+            nums = [0]*len(line)
+            counter = 0
+            stack = []
+            chunks = 0
+            for it, b in enumerate(line):
+                if b in o_brackets:
+                    stack = [b] + stack
+                    counter += 1
+                    #print(" "*counter*2, b)
+                if b in c_brackets:
+                    #print(" "*counter*2, b)
+                    counter -= 1
+                    if bracket_lookup[b] == stack[0]:
+                        chunks += 1
+                        #print(f"chunk {chunks} found of {stack[0]}{b}")
+                        stack.pop(0)
+                    else:
+                        print(f"expected {bracket_lookup[stack[0]]} but got {b}")
+                        points += point_lookup[b]
+                        stack.pop(0)
+
+        print(points)
+                #nums[it] = counter
+            #spaced(line)
+            #spaced(nums)
+
+
 
 
 
