93c707efc74074ade9e314b67061ea9eeb1f8c76
파벨 <pardela.pawel@gmail.com>
2021-12-13 13:30:41 +0100

    solved 13 part 1

--- 13/solution1.py
+++ 13/solution1.py
@@ -1,9 +1,74 @@
 #!/bin/python
 import sys
+from collections import namedtuple
+Point = namedtuple('Point', 'x y')
+
+def print_grid(points, fold_direction=None, edge=None):
+    def draw_point(x, y):
+        if Point(x, y) in points:
+            return "#"
+        else:
+            return '.'
+    points = set(points)
+    max_x = max([x for x, y in points]) + 1
+    max_y = max([y for x, y in points]) + 1
+    grid = [[False]*max_x for _ in range(max_y)]
+    padding = 1
+    print("  " + (" "*(padding-1)), ''.join([str(n).ljust(padding) for n in range(max_x)]))
+    for y in range(max_y):
+        row = str(y).rjust(2) + " "
+        if fold_direction == 'y' and edge == y:
+            row += '='*max_x*padding
+        else:
+            row += (' '*(padding-1)).join([draw_point(x, y) for x in range(max_x)])
+        print(row)
+
 
 def main():
     with open(sys.argv[1]) as input_file:
-        pass
+        points = []
+        folds = []
+        for line in input_file.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            if line.startswith("fold"):
+                fold, num = line.split('=')
+                direction = fold[-1]
+                folds.append((direction, int(num)))
+                continue
+            x, y = [int(n) for n in line.split(',')]
+            points.append(Point(x,y))
+        #print(points)
+        print(folds)
+        max_x = max([x for x, y in points]) + 1
+        max_y = max([y for x, y in points]) + 1
+        print(max_x, max_y)
+        #print_grid(points)
+        for direction, edge in [folds[0]]:
+            if direction == 'y':
+                fold_map = dict(zip(range(edge, max_y + 1), range(edge, -1, -1)))
+            if direction == 'x':
+                fold_map = dict(zip(range(edge, max_x + 1), range(edge, -1, -1)))
+            print(f'Fold along {direction} -> {edge}')
+            print(fold_map)
+            #print_grid(points, direction, edge)
+            new_points = set()
+            for x, y in points:
+                if direction == 'y':
+                    if y < edge:
+                        new_points.add(Point(x, y))
+                    else:
+                        new_points.add(Point(x, fold_map[y]))
+                if direction == 'x':
+                    if x < edge:
+                        new_points.add(Point(x, y))
+                    else:
+                        new_points.add(Point(fold_map[x], y))
+            points = new_points
+            #print_grid(points)
+            print(f'num of points: {len(points)}')
+
 
 
 
